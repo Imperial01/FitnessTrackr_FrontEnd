1991e30ee15f7bb7fd74d47f7fe90db421b6e379
Evelyn <evydelgado18@gmail.com>
2022-09-03 11:27:07 -0600

    finished api, doing com

---
+++ src/components/Activities.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { fetchCreateActivity } from "../utils/api";
+
+const Activities = () => {
+    const [activities, setActivities] = useState([]);
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const history = useHistory()
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if(token) {
+            const createActivity = await fetchCreateActivity(token, name, description)
+            ([createActivity])
+            history.push('/activities')
+        } else {
+            alert("Please Login")
+        }
+    }
+
+    useEffect(() => {
+        fetchCreateActivity()
+    }, [])
+
+
+    return (
+        <>
+            <div id="create-container">
+                <form onSubmit={handleSubmit}>
+                    <h2>Create an Activity</h2>
+                    <label>Name: </label>
+                        <input
+                            type="text"
+                            placeholder="enter activity name"
+                            value={name}
+                            onChange={(event) => {
+                                setName(event.target.value);
+                            }}
+                        ></input>
+                    <label>Description: </label>
+                        <input
+                            type="text"
+                            value={description}
+                            placeholder="enter activity description"
+                            onChange={(event) => {
+                                setDescription(event.target.value);
+                            }}
+                        ></input>
+                    <button type="submit">Add activity</button>
+                </form>
+            </div>
+            {token ? (
+                <>
+                    <CreateActivityForm
+                        activities={activities}
+                        setActivities={setActivities}
+                    />
+                </>
+            ) : null}   
+        </>
+    )
+}
+
+
+export default Activities;
--- src/utils/api.js
+++ src/utils/api.js
@@ -21,3 +21,61 @@ export const fetchRegister = async (username, password) => {
         return(result.token)
     }
 }
+
+export const fetchActivities = async () => {
+    try {
+        const response = await fetch(`${APIURL}/activities`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application.json'
+            },
+        })
+
+    const result = await response.json();
+    console.log(result);
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const fetchCreateActivity = async (name, description) => {
+    try {
+        const response = await fetch(`${APIURL}/activities`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application.json',
+                'Authorization': `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                name: name,
+                description: description
+            })    
+        })
+    
+    const result = await response.json();
+    console.log(result);
+    } catch (error) {
+        console.error('Activity already created', error);
+    }
+}
+
+export const fetchUpdateActivities = async (name, description, activityId) => {
+    try {
+        const response = await fetch(`${APIURL}/Aactivities/${activityId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application.json',
+                'Authorization': `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                name: name,
+                description: description
+            })
+        })
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error('Activity could not be updated', error)
+    }
+}
